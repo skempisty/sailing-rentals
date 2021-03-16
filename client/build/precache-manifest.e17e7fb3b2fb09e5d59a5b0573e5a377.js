ef0326316e1b0c4eb89d7e8885b3b45eae55ce2a
Stephen Kempisty <stephen@edlio.com>
2021-03-16 00:06:00 -0700

    add build folder to git

---
+++ client/build/precache-manifest.e17e7fb3b2fb09e5d59a5b0573e5a377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6a8094b555780868e4ff6d25f4c07e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d8a27ff79531eb63ffd",
+    "url": "/static/css/2.4210231d.chunk.css"
+  },
+  {
+    "revision": "6d8a27ff79531eb63ffd",
+    "url": "/static/js/2.bdeb6c51.chunk.js"
+  },
+  {
+    "revision": "e1f5ad522e1b15731a197887d22f1a58",
+    "url": "/static/js/2.bdeb6c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89dde8a1b47dcc552a41",
+    "url": "/static/js/main.24f2f8a3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
