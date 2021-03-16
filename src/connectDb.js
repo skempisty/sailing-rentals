9f5399555fa1bf8503799d459b4961da65e00c05
Stephen Kempisty <stephen@edlio.com>
2021-03-16 02:41:52 -0700

    maybe just missing property

--- src/connectDb.js
+++ src/connectDb.js
@@ -12,6 +12,7 @@ module.exports = {
     console.log('Connecting to the DB...')
 
     connection = mysql.createPool({
+      connectionLimit : 10,
       host: process.env.DATABASE_HOST,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
