3f91f7385528ef6380d48daaf4ca4f3df3542df3
Stephen Kempisty <stephen@edlio.com>
2021-03-17 02:47:10 -0700

    Add build folder back

---
+++ client/build/precache-manifest.4424332e78b4327522e99a5bcc60709a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a750de6a99178bf7a4b35123458770f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d8a27ff79531eb63ffd",
+    "url": "/static/css/2.4210231d.chunk.css"
+  },
+  {
+    "revision": "6d8a27ff79531eb63ffd",
+    "url": "/static/js/2.bdeb6c51.chunk.js"
+  },
+  {
+    "revision": "e1f5ad522e1b15731a197887d22f1a58",
+    "url": "/static/js/2.bdeb6c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7b271b4e61fef7f431",
+    "url": "/static/js/main.db458b7c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
