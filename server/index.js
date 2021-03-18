10ed829a6ba10820d350d38faf69975c3ac370a3
Stephen Kempisty <stephen@edlio.com>
2021-03-18 13:23:21 -0700

    add backend url filtering for React router

--- server/index.js
+++ server/index.js
@@ -26,6 +26,23 @@
 
   app.use(bodyParser.json())
 
+  /**
+   * Since front/back ends are hosted on same server, we need to handle routes
+   * that would normally just be handled by React Router. Instead of trying to
+   * GET pages, just direct to / and allow React Router to display the correct
+   * view
+   */
+  app.use((req, res, next) => {
+    const apiCall = /^\/api/.test(req.url)
+    const staticAssetCall = /^(\/static|\/favicon|\/manifest)/.test(req.url)
+
+    if (!apiCall && !staticAssetCall) {
+      req.url = '/'
+    }
+
+    next();
+  });
+
   app.use('/api', routes)
 
   if (process.env.NODE_ENV === 'production') {
