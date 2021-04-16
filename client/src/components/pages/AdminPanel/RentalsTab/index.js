7d2657a56ecce0f31db73456c0b79d5a39fb01c4
Stephen Kempisty <stephen@edlio.com>
2021-04-16 03:05:41 -0700

    hide past rentals in admin tab rentals tab when no past rentals exist

--- client/src/components/pages/AdminPanel/RentalsTab/index.js
+++ client/src/components/pages/AdminPanel/RentalsTab/index.js
@@ -42,30 +42,36 @@ class RentalsTab extends React.Component {
           </Table>
         </Card>
 
-        <h3 style={{ color: 'white' }}>Past</h3>
+        {pastRentals.length > 0 &&
+          <React.Fragment>
+            <h3 style={{color: 'white', marginTop: '0.5em'}}>Past</h3>
 
-        <Card>
-          <Table>
-            <thead><tr>
-              <th>Sailor</th>
-              <th>Start</th>
-              <th>End</th>
-              <th>Boat</th>
-              <th>Crew</th>
-              <th>Rented At</th>
-            </tr></thead>
+            <Card>
+              <Table>
+                <thead>
+                <tr>
+                  <th>Sailor</th>
+                  <th>Start</th>
+                  <th>End</th>
+                  <th>Boat</th>
+                  <th>Crew</th>
+                  <th>Rented At</th>
+                </tr>
+                </thead>
 
-            <tbody>
-              {pastRentals.map((rental, index) =>
-                <RentalRow
-                  key={`rental-row-${rental.id}-${index}`}
-                  rental={rental}
-                  showSailor
-                />
-              )}
-            </tbody>
-          </Table>
-        </Card>
+                <tbody>
+                {pastRentals.map((rental, index) =>
+                  <RentalRow
+                    key={`rental-row-${rental.id}-${index}`}
+                    rental={rental}
+                    showSailor
+                  />
+                )}
+                </tbody>
+              </Table>
+            </Card>
+          </React.Fragment>
+        }
       </React.Fragment>
     )
   }
