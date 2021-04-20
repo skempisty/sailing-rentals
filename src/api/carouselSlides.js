d9ff7bdd738e72f8a13c146eadb621c459755bad
Stephen Kempisty <stephen@edlio.com>
2021-04-19 18:16:19 -0700

    add full CRUD endpoints for carousel slides and posts

--- src/api/carouselSlides.js
+++ src/api/carouselSlides.js
@@ -4,9 +4,50 @@ exports.getCarouselSlides = async () => {
   return await db.query(`SELECT * FROM ${db.name}.carousel_slides WHERE deletedAt = NULL`)
 }
 
-// exports.createHomepageCarouselSlide = async ({ label, subText, imgBlob, createdBy }) => {
-//   // post image to S3 - get S3 link
-//   // const imageS3Url = image post and get S3 url
-//
-//   return await db.query('INSERT INTO sailing.homepage_carousel_slides (label, subText, imageUrl, createdBy) VALUES (?, ?, ?, ?)', newSlide)
-// }
+exports.createCarouselSlide = async (createdBy, slideObj) => {
+  const { label, subText, imageUrl } = slideObj
+
+  const newSlide = [ createdBy, label, subText, imageUrl ]
+
+  await db.query(`INSERT INTO ${db.name}.carousel_slides (createdBy, label, subText, imageUrl) VALUES (?, ?, ?, ?)`, newSlide)
+
+  const [ slide ] = await db.query(`SELECT * FROM ${db.name}.carousel_slides WHERE id = LAST_INSERT_ID()`)
+
+  return slide
+}
+
+exports.updateCarouselSlide = async (id, updateFields) => {
+  const { label, subText, imageUrl } = updateFields
+
+  const updateSql = ['updatedAt = CURRENT_TIMESTAMP']
+  const sqlArgs = []
+
+  if (label !== null) {
+    updateSql.push('label = ?')
+    sqlArgs.push(label)
+  }
+
+  if (subText !== null) {
+    updateSql.push('subText = ?')
+    sqlArgs.push(subText)
+  }
+
+  if (imageUrl !== null) {
+    updateSql.push('imageUrl = ?')
+    sqlArgs.push(imageUrl)
+  }
+
+  sqlArgs.push(id)
+
+  const sql = `UPDATE ${db.name}.carousel_slides SET ${updateSql.join(', ')} WHERE id = ?`
+
+  await db.query(sql, sqlArgs)
+
+  const [ slide ] = await db.query(`SELECT * FROM ${db.name}.carousel_slides WHERE id = ?`, [id])
+
+  return slide
+}
+
+exports.deleteCarouselSlide = async (id) => {
+  return await db.query(`UPDATE ${db.name}.carousel_slides SET deletedAt = CURRENT_TIMESTAMP WHERE id = ?`, [id])
+}
--- src/api/posts.js
+++ src/api/posts.js
@@ -3,3 +3,51 @@ const db = require('../connectDb')
 exports.getPosts = async () => {
   return await db.query(`SELECT * FROM ${db.name}.posts WHERE deletedAt = NULL`)
 }
+
+exports.createPost = async (createdBy, postObj) => {
+  const { title, body, imageUrl } = postObj
+
+  const newPost = [ createdBy, title, body, imageUrl ]
+
+  await db.query(`INSERT INTO ${db.name}.posts (createdBy, title, body, imageUrl) VALUES (?, ?, ?, ?)`, newPost)
+
+  const [ post ] = await db.query(`SELECT * FROM ${db.name}.posts WHERE id = LAST_INSERT_ID()`)
+
+  return post
+}
+
+exports.updatePost = async (id, updateFields) => {
+  const { title, body, imageUrl } = updateFields
+
+  const updateSql = ['updatedAt = CURRENT_TIMESTAMP']
+  const sqlArgs = []
+
+  if (title !== null) {
+    updateSql.push('title = ?')
+    sqlArgs.push(title)
+  }
+
+  if (body !== null) {
+    updateSql.push('body = ?')
+    sqlArgs.push(body)
+  }
+
+  if (imageUrl !== null) {
+    updateSql.push('imageUrl = ?')
+    sqlArgs.push(imageUrl)
+  }
+
+  sqlArgs.push(id)
+
+  const sql = `UPDATE ${db.name}.posts SET ${updateSql.join(', ')} WHERE id = ?`
+
+  await db.query(sql, sqlArgs)
+
+  const [ post ] = await db.query(`SELECT * FROM ${db.name}.posts WHERE id = ?`, [id])
+
+  return post
+}
+
+exports.deletePost = async (id) => {
+  return await db.query(`UPDATE ${db.name}.posts SET deletedAt = CURRENT_TIMESTAMP WHERE id = ?`, [id])
+}
