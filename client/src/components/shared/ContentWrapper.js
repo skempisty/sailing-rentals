4114c7a1766dc54a702e0a0b1d7309ea8508ae04
Stephen Kempisty <stephenkempisty@Stephens-MacBook-Pro.local>
2021-05-22 13:35:34 -0700

    make content wrapper responsive

--- client/src/components/shared/ContentWrapper.js
+++ client/src/components/shared/ContentWrapper.js
@@ -1,4 +1,19 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
+
+import { breakpoints } from '../../config'
+
+const ResponsivenessWrapper = styled.div`
+  div.content-wrapper {
+    padding: 2em 2em 8em;
+  }
+
+  @media only screen and (min-width: ${breakpoints.desktop}) {
+    div.content-wrapper {
+      padding: 2em 5em 8em;
+    }
+  }
+`
 
 /**
  * Container component to wrap each page's content for style purposes
@@ -8,15 +23,14 @@ export default class ContentWrapper extends React.Component {
     const { children } = this.props;
 
     return (
-      <div
-        style={{
-          margin: '0 auto',
-          padding: '2em 5em 8em',
-          maxWidth: '92em'
-        }}
-      >
-        {children}
-      </div>
+      <ResponsivenessWrapper>
+        <div
+          className='content-wrapper'
+          style={{ margin: '0 auto', maxWidth: '92em' }}
+        >
+          {children}
+        </div>
+      </ResponsivenessWrapper>
     )
   }
 }
