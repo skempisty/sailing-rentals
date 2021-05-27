600ef32555aba95a24a4df90d1c2d0f5ec40c59a
Stephen Kempisty <stephenkempisty@Stephens-MacBook-Pro.local>
2021-05-27 15:16:06 -0700

    remove no cache header

--- server/index.js
+++ server/index.js
@@ -21,7 +21,6 @@
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Cache-Control", "public, no-cache");
     next();
   });
 
