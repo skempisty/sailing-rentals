b35b973a77e1f85c7f232a73535a27e2744ffa81
Stephen Kempisty <stephen.kempisty@gmail.com>
2021-06-04 00:05:35 -0700

    add settings DAO

---
+++ src/api/settings.js
@@ -0,0 +1,49 @@
+const db = require('../connectDb')
+
+exports.getSettings = async () => {
+  return await getSettingsObject()
+}
+
+exports.updateSettings = async (updatedSettingsObj) => {
+  const settingsNames = Object.keys(updatedSettingsObj)
+
+  /*
+   * Update multiple records in one query
+   * https://stackoverflow.com/a/22492375/11777006
+   */
+  const updateSql = [`UPDATE ${db.name}.settings SET value = CASE name`]
+  const sqlArgs = []
+
+  settingsNames.forEach((settingName) => {
+    const settingValue = updatedSettingsObj[settingName]
+
+    updateSql.push('WHEN ? THEN ?')
+    sqlArgs.push(settingName, settingValue)
+  })
+
+  updateSql.push('END WHERE name IN (')
+
+  const questionMarkArray = new Array(settingsNames.length).fill('?');
+
+  updateSql.push(questionMarkArray.join(','))
+  sqlArgs.push(...settingsNames)
+
+  updateSql.push(')')
+
+  await db.query(updateSql.join(' '), sqlArgs)
+
+  return await getSettingsObject()
+}
+
+async function getSettingsObject() {
+  const settingsArray = await db.query(`SELECT * FROM ${db.name}.settings`)
+
+  // I want a settings object not an array of settings which is the way it comes
+  const settingsObj = {}
+
+  settingsArray.forEach(({ name, value }) => {
+    settingsObj[name] = value
+  })
+
+  return settingsObj
+}
