ffad9cb596b141d9d22c14ddbf958a2e30605e0d
Stephen Kempisty <stephen.kempisty@gmail.com>
2021-06-04 00:07:47 -0700

    add script to add min/max rental hours settings and rename site_settings table to settings

--- scripts/1622445023192_rename_settings_table.js
+++ scripts/1622445023192_rename_settings_table.js
@@ -1,2 +1,15 @@
-ALTER TABLE table_name
-RENAME TO new_table_name;
+(async () => {
+  require('dotenv').config()
+  const db = require('../src/connectDb')
+
+  const SAILING_DB_NAME = process.env.DATABASE_NAME || 'sailing'
+
+  await db.connect()
+
+  await db.query(`ALTER TABLE ${SAILING_DB_NAME}.site_settings RENAME TO settings`)
+
+  await db.query(`INSERT INTO ${db.name}.settings (name, value) VALUES
+    ('min_rental_hours', '3'),
+    ('max_rental_hours', '3')
+  `)
+})()
