d2298457d99a40e8ac935aca43a7d32f5ebe6b5f
Stephen Kempisty <stephen.kempisty@gmail.com>
2021-06-09 17:19:48 -0700

    add describe block

--- src/__test__/api/misc/siteData.test.js
+++ src/__test__/api/misc/siteData.test.js
@@ -16,22 +16,24 @@ afterAll(async () => {
   await teardownTestDb(TEST_DB_NAME)
 })
 
-it('should get non-logged in data when not logged in', async () => {
-  const res = await request.get('/api/site_data')
-    .set('Content-Type', 'application/json')
-    .set('Authorization', 'Bearer null')
+describe('GET /api/site_data', function() {
+  it('should get non-logged in data when not logged in', async () => {
+    const res = await request.get('/api/site_data')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', 'Bearer null')
 
-  const {
-    currentUser,
-    myRentals,
-    allRentals,
-    myPayments,
-    allPayments
-  } = res.body
+    const {
+      currentUser,
+      myRentals,
+      allRentals,
+      myPayments,
+      allPayments
+    } = res.body
 
-  expect(currentUser).toBe(null)
-  expect(myRentals.length).toBe(0)
-  expect(allRentals.length).toBe(0)
-  expect(myPayments.length).toBe(0)
-  expect(allPayments.length).toBe(0)
+    expect(currentUser).toBe(null)
+    expect(myRentals.length).toBe(0)
+    expect(allRentals.length).toBe(0)
+    expect(myPayments.length).toBe(0)
+    expect(allPayments.length).toBe(0)
+  })
 })
