076fe575e419b5d1d753cfa7ac6e1712fa47c1a9
Stephen Kempisty <stephen.kempisty@gmail.com>
2021-06-09 17:29:08 -0700

    Revert "Create main.yml"

--- src/__test__/utils/teardownTestDb.js
+++
@@ -1,7 +0,0 @@
-const db = require('../../../src/connectDb')
-
-module.exports = async function teardownTestDb(testDbName) {
-  await db.query(`DROP DATABASE ${testDbName}`)
-
-  await db.disconnect()
-}
