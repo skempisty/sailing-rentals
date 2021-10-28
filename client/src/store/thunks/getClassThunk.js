7190acd3d94374038f3ab4b75ad370d8aeb9e5a4
Stephen Kempisty <stephen.kempisty@gmail.com>
2021-10-27 19:09:52 -0700

    add boatId to getClass meetings so we can preset the boat selectors on addClass page

--- client/src/store/thunks/getClassThunk.js
+++ client/src/store/thunks/getClassThunk.js
@@ -1,11 +1,24 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 
+import getBoatByRentalId from '../orm/boats/getBoatByRentalId'
+
 const getClassesThunk = createAsyncThunk(
   'classes/getClass',
   async (id, { extra }) => {
     const { classesApiService } = extra
 
-    return await classesApiService.getClass(id)
+    const classDto = await classesApiService.getClass(id)
+
+    /*
+     * Class meetings comes in without an associated boatId
+     * but they do have rentalId if it is a useBoat meeting
+     */
+    classDto.meetings = classDto.meetings.map(mtg => ({
+      boatId: mtg.rentalId ? getBoatByRentalId(mtg.rentalId).id : null,
+      ...mtg
+    }))
+
+    return classDto
   }
 )
 
