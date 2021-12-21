9a8226044230f7c1823c5798d5b7f095c1dc64ec
Stephen Kempisty <stephen.kempisty@gmail.com>
2021-12-20 23:56:27 -0800

    show an alert when youre not registered for any classes

--- client/src/components/pages/ClassRegistration/MyClasses.js
+++ client/src/components/pages/ClassRegistration/MyClasses.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Alert } from 'react-bootstrap'
+
 import Box from '../../shared/styled-system/Box'
 import Title from '../../shared/styled-system/Title'
 import ClassInfoWidget from './ClassInfoWidget'
@@ -9,7 +11,7 @@ import { useSession } from '../../../store/session'
 
 const MyClasses = () => {
   const { currentUser } = useSession()
-  const { inProgressClasses, upcomingClasses, pastClasses, classRegistrations } = useClasses()
+  const { classes, inProgressClasses, upcomingClasses, pastClasses, classRegistrations } = useClasses()
 
   const myRegistrations = classRegistrations.filter(r => r.userId === currentUser.id)
 
@@ -21,44 +23,60 @@ const MyClasses = () => {
   const myUpcomingClasses = upcomingClasses.filter(currentUserIsRegistered)
   const myPastClasses = pastClasses.filter(currentUserIsRegistered)
 
+  const allMyClasses = classes.filter(currentUserIsRegistered)
+
   return (
-    <Box>
-      {myInProgressClasses.length > 0 &&
-        <>
-          <Title as='h2'>In Progress</Title>
+    <>
+      {allMyClasses.length > 0 ?
+        <Box>
+          {myInProgressClasses.length > 0 &&
+            <>
+              <Title as='h2'>In Progress</Title>
 
-          {myInProgressClasses.map((klass) =>
-            <Box key={klass.id} marginBottom='1em'>
-              <ClassInfoWidget klass={klass}/>
-            </Box>
-          )}
-        </>
+              {myInProgressClasses.map((klass) =>
+                <Box key={klass.id} marginBottom='1em'>
+                  <ClassInfoWidget klass={klass}/>
+                </Box>
+              )}
+            </>
+          }
+
+          {myUpcomingClasses.length > 0 &&
+            <>
+              <Title as='h2'>Upcoming</Title>
+
+              {myUpcomingClasses.map((klass) =>
+                <Box key={klass.id} marginBottom='1em'>
+                  <ClassInfoWidget klass={klass}/>
+                </Box>
+              )}
+            </>
+          }
+
+          {myPastClasses.length > 0 &&
+            <>
+              <Title as='h2'>Past</Title>
+
+              {myPastClasses.map((klass) =>
+                <Box key={klass.id} marginBottom='1em'>
+                  <ClassInfoWidget klass={klass}/>
+                </Box>
+              )}
+            </>
+          }
+        </Box>
+        :
+        <Alert
+          variant='dark'
+          style={{
+            margin: '0.5em',
+            textAlign: 'center'
+          }}
+        >
+          <b>Not registered for any classes</b>
+        </Alert>
       }
-
-      {myUpcomingClasses.length > 0 &&
-        <>
-          <Title as='h2'>Upcoming</Title>
-
-          {myUpcomingClasses.map((klass) =>
-            <Box key={klass.id} marginBottom='1em'>
-              <ClassInfoWidget klass={klass}/>
-            </Box>
-          )}
-        </>
-      }
-
-      {myPastClasses.length > 0 &&
-        <>
-          <Title as='h2'>Past</Title>
-
-          {myPastClasses.map((klass) =>
-            <Box key={klass.id} marginBottom='1em'>
-              <ClassInfoWidget klass={klass}/>
-            </Box>
-          )}
-        </>
-      }
-    </Box>
+    </>
   )
 }
 
