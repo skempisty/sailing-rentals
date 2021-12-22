3b23c80ba5f9e0ee7db44717c5994ce4282a00e9
Stephen Kempisty <stephen.kempisty@gmail.com>
2021-12-21 18:50:36 -0800

    update meeting info modal

--- client/src/components/pages/ClassRegistration/ClassInfoWidget/MtgInfoModal.js
+++ client/src/components/pages/ClassRegistration/ClassInfoWidget/MtgInfoModal.js
@@ -2,23 +2,54 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { Modal } from 'react-bootstrap'
+import { FaInfoCircle } from 'react-icons/fa'
+
+import Flex from '../../../shared/styled-system/Flex'
+import Text from '../../../shared/styled-system/Text'
 
 const MtgInfoModal = ({ mtg, show, onHide }) => {
   return (
     <Modal show={show} onHide={onHide}>
       <Modal.Header closeButton>
         <Modal.Title style={{ display: 'flex', alignItems: 'center' }}>
-          <span style={{ marginLeft: '0.5em' }}>Meeting Info</span>
+          <Flex alignItems='center'>
+            <FaInfoCircle/>
+
+            <Text marginLeft='0.5em'>Meeting Details</Text>
+          </Flex>
         </Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
+        {!!mtg.rentalId &&
+          <>
+            <Flex flexDirection='column'>
+              <Text fontWeight='bold'>Address</Text>
+              <Text>Coast Guard Pier</Text>
+              <Text>100 Lighthouse Ave.</Text>
+              <Text>Monterey CA, 93940</Text>
+            </Flex>
 
+            <Flex flexDirection='column' marginTop='1em'>
+              <Text fontWeight='bold'>Gate Code</Text>
+              <Text>2+4,5,1</Text>
+            </Flex>
+
+            {!!mtg.details &&
+              <Flex flexDirection='column' marginTop='1em'>
+                <Text fontWeight='bold'>Additional Details</Text>
+                <Text>{mtg.details}</Text>
+              </Flex>
+            }
+          </>
+        }
+
+        {!mtg.rentalId &&
+          <Flex>
+            {mtg.details ? mtg.details : 'No further details given.'}
+          </Flex>
+        }
       </Modal.Body>
-
-      <Modal.Footer>
-
-      </Modal.Footer>
     </Modal>
   )
 }
