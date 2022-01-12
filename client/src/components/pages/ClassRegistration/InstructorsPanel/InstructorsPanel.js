20b9ee7e5b02aee91db32b385eeb48a76f526bdb
Stephen Kempisty <stephen.kempisty@gmail.com>
2022-01-11 19:15:45 -0800

    add InstructorSPanel tab

--- client/src/components/pages/ClassRegistration/InstructorsPanel/InstructorsPanel.js
+++ client/src/components/pages/ClassRegistration/InstructorsPanel/InstructorsPanel.js
@@ -1,49 +1,65 @@
 import React from 'react'
 
 import { Alert } from 'react-bootstrap'
-import { FaArrowCircleDown } from 'react-icons/fa'
 
-import Box from '../../shared/styled-system/Box'
-import Flex from '../../shared/styled-system/Flex'
-import Title from '../../shared/styled-system/Title'
-import Text from '../../shared/styled-system/Text'
-import ClassInfoWidget from './ClassInfoWidget'
+import Box from '../../../shared/styled-system/Box'
+import Title from '../../../shared/styled-system/Title'
+import ClassInfoWidget from '../ClassInfoWidget'
 
-import isNotDeleted from '../../../utils/isNotDeleted'
-
-import { useClasses } from '../../../store/classes'
+import { useClasses } from '../../../../store/classes'
+import { useSession } from '../../../../store/session'
 
 const InstructorsPanel = () => {
-  const { upcomingClasses } = useClasses()
+  const { currentUser } = useSession()
+  const { inProgressClasses, upcomingClasses } = useClasses()
+
+  const currentUserIsClassInstructor = (klass) => {
+    return klass.meetings.some(m => m.instructorId === currentUser.id)
+  }
+
+  const myInProgressClasses = inProgressClasses.filter(currentUserIsClassInstructor)
+  const myUpcomingClasses = upcomingClasses.filter(currentUserIsClassInstructor)
 
   return (
-    <Box>
-      <Title as='h2'>Assignments</Title>
+    <>
+      {(myInProgressClasses.length > 0 || myUpcomingClasses.length > 0) ?
+        <Box>
+          {myInProgressClasses.length > 0 &&
+            <>
+              <Title as='h3'>In Progress Assignments</Title>
 
-      {upcomingClasses.length > 0 ?
-        <>
-          {upcomingClasses.filter(isNotDeleted).map((klass) =>
-            <Box key={klass.id} marginBottom='1em'>
-              <ClassInfoWidget
-                klass={klass}
-                hasRegisterBtn
-              />
-            </Box>
-          )}
-        </>
+              {myInProgressClasses.map((klass) =>
+                <Box key={klass.id} marginBottom='1em'>
+                  <ClassInfoWidget klass={klass} instructorId={currentUser.id}/>
+                </Box>
+              )}
+            </>
+          }
+
+          {myUpcomingClasses.length > 0 &&
+            <>
+              <Title as='h3'>Upcoming Assignments</Title>
+
+              {myUpcomingClasses.map((klass) =>
+                <Box key={klass.id} marginBottom='1em'>
+                  <ClassInfoWidget klass={klass} instructorId={currentUser.id}/>
+                </Box>
+              )}
+            </>
+          }
+        </Box>
         :
         <Alert
           variant='dark'
           style={{
-            marginTop: '0.5em',
+            margin: '0.5em',
             textAlign: 'center'
           }}
         >
-          <Title as='h3'>No Upcoming Classes</Title>
-          <Text>Please check back later</Text>
+          <b>No assignments</b>
         </Alert>
       }
-    </Box>
+    </>
   )
 }
 
